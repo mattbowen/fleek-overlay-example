69e20b9e9393709b2ddd0d12e13646f47e92b5c1
Matt <matt@mattbowen.net>
2023-04-30 13:18:57 -0400

    Inital, unejected version

---
+++ fedora/fedora.nix
@@ -0,0 +1,27 @@
+{ pkgs, misc, ... }: {
+    home.username = "matt";
+    home.homeDirectory = "/home/matt";
+    programs.git = {
+        enable = true;
+        aliases = {
+            pushall = "!git remote | xargs -L1 git push --all";
+            graph = "log --decorate --oneline --graph";
+            add-nowhitespace = "!git diff -U0 -w --no-color | git apply --cached --ignore-whitespace --unidiff-zero -";
+        };
+        userName = "Matt";
+        userEmail = "matt@mattbowen.net";
+        extraConfig = {
+            feature.manyFiles = true;
+            init.defaultBranch = "main";
+            gpg.format = "ssh";
+        };
+
+        signing = {
+            key = "";
+            signByDefault = builtins.stringLength "" > 0;
+        };
+
+        lfs.enable = true;
+        ignores = [ ".direnv" "result" ];
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Fleek Configuration";
+
+  inputs = {
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Fleek
+    fleek.url = "github:ublue-os/fleek";
+
+  };
+
+  outputs = { nixpkgs, home-manager, fleek, ... }@inputs: {
+
+    # Available through 'home-manager --flake .#your-username@your-hostname'
+    homeConfigurations = {
+    
+      "matt@fedora" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+
+        modules = [ 
+          ./home.nix 
+          ./path.nix
+          ./shell.nix
+          ./user.nix
+          ./aliases.nix
+          ./programs.nix
+          # Host Specific configs
+          ./fedora/fedora.nix
+          ./fedora/user.nix
+          # self-manage fleek
+          {
+           home.packages = [
+            fleek.packages.x86_64-linux.default
+          ];
+          }
+
+        ];
+      };
+      
+    };
+  };
+}
